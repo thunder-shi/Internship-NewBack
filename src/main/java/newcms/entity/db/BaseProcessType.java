75cf655b880b060f0925a983c74d6f1628c6808b
lzh <1185312007@qq.com>
2025-12-31 16:32:20 +0800

    [实习流程模板]完成

---
+++ src/main/java/newcms/entity/db/BaseProcessType.java
@@ -0,0 +1,12 @@
+package newcms.entity.db;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Entity;
+import newcms.entity.base.NameRemarkOrderInfo;
+
+@Getter
+@Setter
+@Entity
+public class BaseProcessType extends NameRemarkOrderInfo {
+}
