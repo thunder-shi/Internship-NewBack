4489be3d54dc52072d0a32b2280aaecd5031900d
thunder10 <thunder10@163.com>
2026-01-01 17:05:23 +0800

    增加审核要求模板管理

---
+++ src/main/java/newcms/entity/db/BaseVerifyType.java
@@ -0,0 +1,18 @@
+package newcms.entity.db;
+
+import newcms.entity.base.NameRemarkOrderInfo;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Entity;
+
+/**
+ * 验证类型表
+ */
+@Getter
+@Setter
+@Entity
+public class BaseVerifyType extends NameRemarkOrderInfo {
+
+}
+
---
+++ src/main/java/newcms/repository/db/BaseVerifyTypeDao.java
@@ -0,0 +1,10 @@
+package newcms.repository.db;
+
+import newcms.entity.db.BaseVerifyType;
+import newcms.repository.base.BaseDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BaseVerifyTypeDao extends BaseDao<BaseVerifyType, Integer> {
+}
+
