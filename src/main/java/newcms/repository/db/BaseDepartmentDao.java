4829f2e4638221631db4cc9a0b4fb7059c50dbe3
lzh <1185312007@qq.com>
2026-01-08 15:37:51 +0800

    [用户管理]批量导入：部门匹配规则

--- src/main/java/newcms/repository/db/BaseDepartmentDao.java
+++ src/main/java/newcms/repository/db/BaseDepartmentDao.java
@@ -2,11 +2,20 @@ package newcms.repository.db;
 
 import newcms.entity.db.BaseDepartment;
 import newcms.repository.base.BaseDao;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BaseDepartmentDao extends BaseDao<BaseDepartment, Integer> {
     Object getByCode(String code);
     Object getByCodeAndIsDeletedFalse(String code);
+    
+    @Query("select t from BaseDepartment t where t.name = ?1")
+    List<BaseDepartment> findByName(String name);
+    
+    @Query("select t from BaseDepartment t where t.name = ?1 and t.isDeleted = false")
+    List<BaseDepartment> findByNameAndIsDeletedFalse(String name);
 }
 
